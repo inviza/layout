3b176e7298a8698c1afe3e62495d53fe283b44f8
inviza <inviza777@gmail.com>
2018-05-28 01:53:16 +0300

    js3 open

--- JS/module-03/js.js
+++ JS/module-03/js.js
@@ -2,40 +2,36 @@
 
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-let login = prompt("Введите пароль пожалуйста");
+let login = prompt("Введите логин пожалуйста");
 
-
-let addLogin = function (logins, login){
-  let checkLoginValidity= function(login){
-        if(login.length>4&&login.length<16){
-            return true;
-        }
-        else{
-            return false;
-        }
-        }
-    if(сheckLoginValidity(login)===true){
-        let checkIfLoginExists=function(logins, login){
-            for(let i=0;i<logins.length;i++){
-                if(logins[i]===login){
-                    return true;
-                } else{
-                    return false;
-                }
-            }
-        }
-        if(сheckIfLoginExists(logins, login)===true){
-            alert('Такой логин уже используется!');
-        }
-        else{
-            logins.push(login);
-            alert('Логин успешно добавлен!');
-        }
-    }
-    else{
-        alert('Ошибка! Логин должен быть от 4 до 16 символов');
-        break;
+let checkLoginValidity = function (login) {
+    if (login.length > 4 && login.length < 16) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-addLogin(logins,login);
+let checkIfLoginExists = function (logins, login) {
+    if (logins.includes(login)) {
+        return true
+    } else {
+        return false
+    }
+}
+let addLogin = function (logins, login) {
+    if (checkLoginValidity(login) === true) {
+       
+        if (checkIfLoginExists(logins, login) === true) {
+            alert('Такой логин уже используется!');
+        } else {
+            logins.push(login);
+            alert('Логин успешно добавлен!');
+        }
+    } else {
+        alert('Ошибка! Логин должен быть от 4 до 16 символов');
+        return;
+    }
+}
+
+addLogin(logins, login);
